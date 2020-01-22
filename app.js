ecf22340f8af60e765d72df19ceeece7c54f9555
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-01-22 18:33:07 +0200

    Add Product Model

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
-const path = require('path');
-const mongoose = require('mongoose');
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById('5e1f642b0a1e7693cca17ada')
-  .then(user => {
-    req.user = new User(user.username, user.email, user.cart, user._id);
-    next();
-  })
-  .catch(err => {
-    console.log(err);
-  })
-})
+  User.findById("5e1f642b0a1e7693cca17ada")
+    .then(user => {
+      req.user = new User(user.username, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
-
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://Muhammad:WuRr5nIhlPGHii8B@cluster0-hebyh.mongodb.net/test?retryWrites=true&w=majority').then((result) => {
-  app.listen(3000);
-}).catch(err => console.log(err));
+mongoose
+  .connect(
+    "mongodb+srv://Muhammad:WuRr5nIhlPGHii8B@cluster0-hebyh.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => console.log(err));

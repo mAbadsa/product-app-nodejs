72a8a12a53dd8238a1e56e6efc63edaaa5b99d0f
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-02-12 16:00:46 +0200

    Add Signup page and Some Modifications on Navigation page

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,42 @@
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  // const isLoggedin = req.get('cookie').split(';')[0].trim().split('=')[1];
-  // console.log(isLoggedin);
-  // console.log(req.get('cookie').split(';')[0].trim().split('=')[1]);
-  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: req.session.isLoggedIn
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("5e4152d47754c42900038a48")
+  User.findById("5bab316ce0a7c75f783cb8a8")
     .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
       req.session.save(err => {
         console.log(err);
         res.redirect("/");
-      })
+      });
     })
     .catch(err => console.log(err));
+};
+
+exports.postSignup = async (req, res, next) => {
+  const { email, password, confirmPassword } = req.body;
+  const user = await new User(email, password, confirmPassword);
+  try {
+    user.save();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 exports.postlogout = (req, res, next) => {

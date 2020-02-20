9c0f8582e70bb043a9ad701c7deec508d3bb2667
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-02-20 13:58:16 +0200

    Validation Product Adding

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,18 +1,37 @@
 const Product = require("../models/product");
+const { validationResult } = require('express-validator');
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    editing: false
+    editing: false,
+    hasError: false,
+    errorMessage: null
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
+  const { title, description, imageUrl, price } = req.body;
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      editing: false,
+      product: {
+        title,
+        imageUrl,
+        price,
+        description
+      },
+      hasError: true,
+      errorMessage: errors.array()[0].msg
+    });
+  }
+
   const product = new Product({
     title: title,
     price: price,
@@ -47,7 +66,9 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: "Edit Product",
         path: "/admin/edit-product",
         editing: editMode,
-        product: product
+        product: product,
+        hasError: false,
+        errorMessage: null
       });
     })
     .catch(err => console.log(err));
@@ -59,6 +80,23 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      editing: false,
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDesc
+      },
+      hasError: true,
+      errorMessage: errors.array()[0].msg
+    });
+  }
 
   Product.findById(prodId)
     .then(product => {

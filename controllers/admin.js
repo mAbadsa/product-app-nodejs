2c1731924a66f2a134f63f9bd3aa8f95b1f7569b
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-03-01 23:12:09 +0200

    ending  Handling Background Requests

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
 
-const fileHelper = require('../util/file');
+const fileHelper = require("../util/file");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
@@ -19,7 +19,7 @@ exports.postAddProduct = (req, res, next) => {
   const image = req.file;
   const errors = validationResult(req);
 
-  if(!image) {
+  if (!image) {
     return res.status(422).render("admin/edit-product", {
       pageTitle: "Add Product",
       path: "/admin/add-product",
@@ -30,13 +30,12 @@ exports.postAddProduct = (req, res, next) => {
         description
       },
       hasError: true,
-      errorMessage: 'Attached file is not an iamge.',
-      validationErrors: [],
-      isLoaded: true
+      errorMessage: "Attached file is not an iamge.",
+      validationErrors: []
     });
   }
 
-  if(!errors.isEmpty()) {
+  if (!errors.isEmpty()) {
     return res.status(422).render("admin/edit-product", {
       pageTitle: "Add Product",
       path: "/admin/add-product",
@@ -49,7 +48,6 @@ exports.postAddProduct = (req, res, next) => {
       hasError: true,
       errorMessage: errors.array()[0].msg,
       validationErrors: errors.array(),
-      isLoaded: true
     });
   }
 
@@ -129,7 +127,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedDesc = req.body.description;
   const errors = validationResult(req);
 
-  if(!errors.isEmpty()) {
+  if (!errors.isEmpty()) {
     return res.status(422).render("admin/edit-product", {
       pageTitle: "Add Product",
       path: "/admin/add-product",
@@ -153,7 +151,7 @@ exports.postEditProduct = (req, res, next) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
-      if(image) {
+      if (image) {
         fileHelper.deleteFile(product.imageUrl);
         product.imageUrl = image.path;
       }
@@ -188,22 +186,20 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
-  .then(product => {
-    if(!product) {
-      return next(new Error("Product not found."));
-    }
-    fileHelper.deleteFile(product.imageUrl);
-    return Product.deleteOne({ _id: prodId, userId: req.user._id });
-  })
-  .then(() => {
-    res.redirect("/admin/products");
-  })
-  .catch(err => {
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
-  });    
+    .then(product => {
+      if (!product) {
+        return next(new Error("Product not found."));
+      }
+      fileHelper.deleteFile(product.imageUrl);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id });
+    })
+    .then(() => {
+      res.status(200).json({ message: "Success!" });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Deleting product failed." });
+    });
 };
